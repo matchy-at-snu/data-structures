f47328e45b21b329d34694db148a5b181b923e78
matchy233 <matchy233@gmail.com>
2021-10-02 01:26:42 +0900

    Reformat code

--- assignments/1/src/main/java/Polynomial.java
+++ assignments/1/src/main/java/Polynomial.java
@@ -7,50 +7,50 @@ import java.util.TreeMap;
 
 public class Polynomial {
 
-  // Create an empty polynomial
-  public Polynomial() {
-    polynomial = new TreeMap<>(Collections.reverseOrder());
-  }
-
-  // Create a single-term polynomial
-  public Polynomial(int coef, int exp) {
-    polynomial = new TreeMap<>(Collections.reverseOrder());
-    polynomial.put(exp, coef);
-  }
-
-  // Add opnd to 'this' polynomial; 'this' is returned
-  public Polynomial add(Polynomial opnd) {
-    for (var entry : opnd.getPolynomialEntries()) {
-      polynomial.compute(entry.getKey(), (k, v) -> (
-        (v == null) ? entry.getValue() : v + entry.getValue()
-      ));
+    // Create an empty polynomial
+    public Polynomial() {
+        polynomial = new TreeMap<>(Collections.reverseOrder());
     }
-    return this;
-  }
 
-  // Subtract opnd from 'this' polynomial; 'this' is returned
-  public Polynomial sub(Polynomial opnd) {
-    for (var entry : opnd.getPolynomialEntries()) {
-      polynomial.compute(entry.getKey(), (k, v) -> (
-              (v == null) ? entry.getValue() : v - entry.getValue()
-      ));
+    // Create a single-term polynomial
+    public Polynomial(int coef, int exp) {
+        polynomial = new TreeMap<>(Collections.reverseOrder());
+        polynomial.put(exp, coef);
     }
-    return this;
-  }
 
-  // Print the terms of 'this' polynomial in decreasing order of exponents.
-  // No pair of terms can share the same exponent in the printout.
-  public void print() {
-    for (var entry : polynomial.entrySet()) {
-      if (entry.getValue() != 0) {
-        System.out.print(entry.getValue() + " " + entry.getKey() + " ");
-      }
+    // Add opnd to 'this' polynomial; 'this' is returned
+    public Polynomial add(Polynomial opnd) {
+        for (var entry : opnd.getPolynomialEntries()) {
+            polynomial.compute(entry.getKey(), (k, v) -> (
+                    (v == null) ? entry.getValue() : v + entry.getValue()
+            ));
+        }
+        return this;
     }
-  }
 
-  private final TreeMap<Integer, Integer> polynomial;
+    // Subtract opnd from 'this' polynomial; 'this' is returned
+    public Polynomial sub(Polynomial opnd) {
+        for (var entry : opnd.getPolynomialEntries()) {
+            polynomial.compute(entry.getKey(), (k, v) -> (
+                    (v == null) ? entry.getValue() : v - entry.getValue()
+            ));
+        }
+        return this;
+    }
 
-  public Set<Map.Entry<Integer, Integer>> getPolynomialEntries() {
-    return polynomial.entrySet();
-  }
+    // Print the terms of 'this' polynomial in decreasing order of exponents.
+    // No pair of terms can share the same exponent in the printout.
+    public void print() {
+        for (var entry : polynomial.entrySet()) {
+            if (entry.getValue() != 0) {
+                System.out.print(entry.getValue() + " " + entry.getKey() + " ");
+            }
+        }
+    }
+
+    private final TreeMap<Integer, Integer> polynomial;
+
+    public Set<Map.Entry<Integer, Integer>> getPolynomialEntries() {
+        return polynomial.entrySet();
+    }
 }
--- assignments/1/src/test/java/MainPolyTest.java
+++ assignments/1/src/test/java/MainPolyTest.java
@@ -4,8 +4,8 @@ import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -16,17 +16,12 @@ public class MainPolyTest {
     static private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     static private List<String> testCases;
 
-
     @BeforeAll
     static void init() {
         System.setOut(new PrintStream(outputStream));
         testCases = IntStream.rangeClosed(1, 9)
                 .mapToObj(num -> ("test0" + num))
                 .collect(Collectors.toList());
-        Assertions.assertArrayEquals(
-                new String[]{"test01", "test02", "test03", "test04", "test05", "test06", "test07", "test08", "test09"},
-                testCases.toArray()
-                );
     }
 
     @Test
