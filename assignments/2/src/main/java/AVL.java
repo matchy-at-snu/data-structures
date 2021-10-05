a9b1bb38f5f4dfc72eaa2d26005ec2d5198bc47c
matchy233 <matchy233@gmail.com>
2021-10-06 04:43:38 +0900

    Reformat code

--- assignments/2/src/main/java/AVL.java
+++ assignments/2/src/main/java/AVL.java
@@ -1,11 +1,13 @@
 // AVL Binary Search Tree
 // Bongki Moon (bkmoon@snu.ac.kr)
-  
-public class AVL extends BST
-{
-  public AVL() { }
-  
-  public void insert(String key) { }
- 
+
+public class AVL extends BST {
+
+    public AVL() {
+    }
+
+    public void insert(String key) {
+    }
+
 }
 
--- assignments/2/src/main/java/BST.java
+++ assignments/2/src/main/java/BST.java
@@ -3,23 +3,48 @@
 
 public class BST { // Binary Search Tree implementation
 
-  protected boolean NOBSTified = false;
-  protected boolean OBSTified = false;
+    protected boolean NOBSTified = false;
+    protected boolean OBSTified = false;
 
-  public BST() { }
+    public BST() {
+    }
 
-  public int size() { return 0; }
-  public void insert(String key) { }
-  public boolean find(String key) { return true; }
+    public int size() {
+        return 0;
+    }
 
-  public int sumFreq() { return 0; }
-  public int sumProbes() { return 0; }
-  public int sumWeightedPath() { return 0; }
-  public void resetCounters() { }
+    public void insert(String key) {
+    }
 
-  public void nobst() { }	// Set NOBSTified to true.
-  public void obst() { }	// Set OBSTified to true.
-  public void print() { }
+    public boolean find(String key) {
+        return true;
+    }
+
+    public int sumFreq() {
+        return 0;
+    }
+
+    public int sumProbes() {
+        return 0;
+    }
+
+    public int sumWeightedPath() {
+        return 0;
+    }
+
+    public void resetCounters() {
+    }
+
+    public void nobst() {
+        NOBSTified = true;
+    }    // Set NOBSTified to true.
+
+    public void obst() {
+        OBSTified = true;
+    }    // Set OBSTified to true.
+
+    public void print() {
+    }
 
 }
 
