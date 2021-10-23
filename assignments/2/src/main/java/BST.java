f5c77a82cd95822ae2a9754db25bda0acab4a73f
matchy233 <matchy233@gmail.com>
2021-10-24 06:14:41 +0900

    Implement BST insert

--- assignments/2/src/main/java/BST.java
+++ assignments/2/src/main/java/BST.java
@@ -1,12 +1,17 @@
 // (Nearly) Optimal Binary Search Tree
 // Bongki Moon (bkmoon@snu.ac.kr)
+// Modified by Matchy
 
-public class BST { // Binary Search Tree implementation
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
 
+public class BST extends AbstractBST { // Binary Search Tree implementation
     protected boolean NOBSTified = false;
     protected boolean OBSTified = false;
 
     public BST() {
+        super();
     }
 
     public int size() {
@@ -14,6 +19,32 @@ public class BST { // Binary Search Tree implementation
     }
 
     public void insert(String key) {
+        Node node = new Node(key);
+        if (root == null) {
+            root = node;
+        } else {
+            _insert(root, node);
+        }
+    }
+
+    private void _insert(Node root, Node node) {
+        if (node.compareTo(root) < 0) { //
+            if (root.left == null) {
+                root.left = node;
+                node.parent = root;
+            } else {
+                _insert(root.left, node);
+            }
+        } else if (node.compareTo(root) > 0) {
+            if (root.right == null) {
+                root.right = node;
+                node.parent = root;
+            } else {
+                _insert(root.right, node);
+            }
+        } else {
+            root.freq = root.freq + 1;
+        }
     }
 
     public boolean find(String key) {
@@ -44,6 +75,26 @@ public class BST { // Binary Search Tree implementation
     }    // Set OBSTified to true.
 
     public void print() {
+    }
+
+    /**
+     * Returns all keys in the tree as an {@code Iterable} in increasing order
+     * To iterate over all the keys in the tree named {@code bst},
+     * use the foreach notation: {@code for (String key : bst.keys())}.
+     *
+     * @return all keys in the binary search tree in increasing order (preorder)
+     */
+    public Iterable<String> keys() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        keys(root, arrayList);
+        return arrayList;
+    }
+
+    private void keys(Node x, ArrayList<String> arrayList) {
+        if (x == null) return;
+        keys(x.left, arrayList);
+        arrayList.add(x.key);
+        keys(x.right, arrayList);
     }
 
 }
