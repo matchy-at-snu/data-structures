f4a6a50b344140a685aa710e9916e6dc1585e76f
matchy233 <matchy233@gmail.com>
2021-10-24 02:05:38 +0900

    Update AbstractBST with Javadoc comments

--- assignments/2/src/main/java/AbstractBST.java
+++ assignments/2/src/main/java/AbstractBST.java
@@ -1,47 +1,13 @@
+// Created by Matchy
 
 public abstract class AbstractBST {
-    public static class Node implements Comparable<Node> {
-        private Node left;
-        private Node right;
-        private final String key;
-        private int freq;
-        private int accessCount;
-
-        public Node getLeft() {
-            return left;
-        }
-
-        public void setLeft(Node left) {
-            this.left = left;
-        }
-
-        public Node getRight() {
-            return right;
-        }
-
-        public void setRight(Node right) {
-            this.right = right;
-        }
-
-        public String getKey() {
-            return key;
-        }
-
-        public int getFreq() {
-            return freq;
-        }
-
-        public void setFreq(int freq) {
-            this.freq = freq;
-        }
-
-        public int getAccessCount() {
-            return accessCount;
-        }
-
-        public void setAccessCount(int accessCount) {
-            this.accessCount = accessCount;
-        }
+    protected static class Node implements Comparable<Node> {
+        protected Node parent;
+        protected Node left;
+        protected Node right;
+        protected final String key;
+        protected int freq;
+        protected int accessCount;
 
         public Node(String key) {
             this.left = null;
@@ -59,8 +25,66 @@ public abstract class AbstractBST {
         @Override
         public String toString() {
             return "[" + this.key + ":" +
-                    this.freq + ":" +
-                    this.accessCount + "]";
+                         this.freq + ":" +
+                         this.accessCount + "]";
         }
     }
+
+    protected Node root;
+
+    public AbstractBST() {
+        this.root = null;
+    }
+
+    public abstract int size();
+
+    /**
+     * Inserts a key into the tree using the standard BST insertion algorithm.
+     * If the same key exists already in the tree, increment its frequency
+     * @param key the key to be inserted into the tree
+     */
+    public abstract void insert(String key);
+
+    public abstract boolean find(String key);
+
+    /**
+     * @return the sum of frequency of all the keys in the tree
+     */
+    public abstract int sumFreq();
+
+    /**
+     * @return the sum of access count of all the keys in the tree
+     */
+    public abstract int sumProbes();
+
+    /**
+     * Returns the sum of weighted path lengths of the tree. The weighted path
+     * length of a node is calculated by weight * (1 + level), where weight refers
+     * to the frequency of its key
+     * @return the sum of weighted path lengths of the tree
+     */
+    public abstract int sumWeightedPath();
+
+    /**
+     * Resets both the frequencies and access counts for all the keys in the tree
+     */
+    public abstract void resetCounters();
+
+    /**
+     * Converts the tree from a plain BST into a Nearly Optimal BST. The
+     * frequencies of the trees are used as weights
+     */
+    public abstract void nobst();
+
+    /**
+     * Converts the tree from a plain BST into a Nearly Optimal BST. The
+     * frequencies of the trees are used as weights
+     */
+    public abstract void obst();
+
+    /**
+     * Prints the keys in the tree in the increasing order of their values.
+     * Each key should appear on a separate line and
+     */
+    public abstract void print();
 }
