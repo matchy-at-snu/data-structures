2a57f6989a4102e2d35ce72248476dc12ce4a330
matchy233 <matchy233@gmail.com>
2021-10-24 09:01:11 +0900

    Fix parent tracing issue in AVL

--- assignments/2/src/main/java/AVL.java
+++ assignments/2/src/main/java/AVL.java
@@ -20,12 +20,36 @@ public class AVL extends BST {
     }
 
     private Node _insert(Node root, Node node) {
-        if (root == null) {
-            root = node;
-            root.height = 1;
-            this.size++;
-        } else if (node.compareTo(root) > 0) {
-            root.right = _insert(root.right, node);
+
+        if (node.compareTo(root) < 0) { // should insert to right subtree
+            if (root.left == null) {
+                root.left = node;
+                node.parent = root;
+                this.size++;
+            } else {
+                root.left = _insert(root.left, node);
+            }
+            // Balance
+            if (height(root.left) - height(root.right) == 2) {
+                if (node.compareTo(root.left) < 0) {
+                    Node parent = root.parent;
+                    root = rotateRight(root);
+                    root.parent = parent;
+                } else {
+                    Node parent = root.parent;
+                    root = rotateLeftRight(root);
+                    root.parent = parent;
+                }
+            }
+        } else if (node.compareTo(root) > 0) { // should insert to right subtree
+            if (root.right == null) {
+                root.right = node;
+                node.parent = root;
+                this.size++;
+            } else {
+                root.right = _insert(root.right, node);
+            }
+            // Balance
             if (height(root.right) - height(root.left) == 2) {
                 if (node.compareTo(root.right) >= 0) {
                     root = rotateLeft(root);
@@ -33,16 +57,7 @@ public class AVL extends BST {
                     root = rotateRightLeft(root);
                 }
             }
-        } else if (node.compareTo(root) < 0) {
-            root.left = _insert(root.left, node);
-            if (height(root.left) - height(root.right) == 2) {
-                if (node.compareTo(root.left) < 0) {
-                    root = rotateRight(root);
-                } else {
-                    root = rotateLeftRight(root);
-                }
-            }
-        } else {
+        } else { // already in the tree
             root.freq = root.freq + 1;
         }
         root.height = 1 + Math.max(height(root.right), height(root.right));
@@ -61,16 +76,29 @@ public class AVL extends BST {
 
     private Node rotateRight(Node root) {
         Node newRoot = root.left;
+
         root.left = newRoot.right;
+        if (newRoot.right != null) {
+            newRoot.right.parent = root;
+        }
+
         newRoot.right = root;
+        newRoot.parent = root.parent;
         root.parent = newRoot;
+
         return newRoot;
     }
 
     private Node rotateLeft(Node root) {
         Node newRoot = root.right;
+
         root.right = newRoot.left;
+        if (newRoot.left != null) {
+            newRoot.left.parent = root;
+        }
+
         newRoot.left = root;
+        newRoot.parent = root.parent;
         root.parent = newRoot;
         return newRoot;
     }
