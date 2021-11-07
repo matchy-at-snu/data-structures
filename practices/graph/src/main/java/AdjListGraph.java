065fe235f1ef5b5abaa608f79eb2ffc479ceb5df
matchy233 <matchy233@gmail.com>
2021-11-07 13:29:52 -0500

    Implement Graph by adjacent list

--- practices/graph/src/main/java/AdjListGraph.java
+++ practices/graph/src/main/java/AdjListGraph.java
@@ -1,4 +1,60 @@
+import java.util.*;
 
-class AdjListGraph extends Graph {
+class AdjListGraph<T> implements Graph<T> {
 
+    private HashMap<T, LinkedList<T>> adjacentList;
+
+    public AdjListGraph() {
+       adjacentList = new HashMap<>();
+    }
+
+    @Override
+    public void addEdge(T startVertex, T endVertex) {
+        try {
+            adjacentList.get(startVertex).add(endVertex);
+        } catch (NullPointerException e) {
+            adjacentList.put(startVertex, new LinkedList<>(Arrays.asList(endVertex)));
+        }
+
+        try {
+            adjacentList.get(endVertex).add(startVertex);
+        } catch (NullPointerException e) {
+            adjacentList.put(endVertex, new LinkedList<>(Arrays.asList(startVertex)));
+        }
+    }
+
+    @Override
+    public Iterable<T> DFS(T startVertex) {
+        ArrayList<T> visited = new ArrayList<>(adjacentList.size());
+        _dfs(startVertex, visited);
+        return visited;
+    }
+
+    private void _dfs(T startVertex, List<T> visited) {
+        visited.add(startVertex);
+        for (T adjacentVertex : adjacentList.get(startVertex)) {
+            if (!visited.contains(adjacentVertex)) {
+                _dfs(adjacentVertex, visited);
+            }
+        }
+    }
+
+    @Override
+    public Iterable<T> BFS(T startVertex) {
+        ArrayList<T> visited = new ArrayList<>(adjacentList.size());
+        Queue<T> queue = new LinkedList<>();
+        queue.add(startVertex);
+        visited.add(startVertex);
+        while (!queue.isEmpty()) {
+            T vertex = queue.poll();
+            visited.add(vertex);
+            for (T adjacentVertex : adjacentList.get(vertex)) {
+                if (!visited.contains(adjacentVertex)) {
+                    queue.add(adjacentVertex);
+                    visited.add(adjacentVertex);
+                }
+            }
+        }
+        return visited;
+    }
 }
--- practices/graph/src/main/java/Graph.java
+++ practices/graph/src/main/java/Graph.java
@@ -1,8 +1,5 @@
-
-abstract class Graph {
-    abstract void addEdge(int v, int w);
-    abstract Iterable<Integer> adj(int v);
-    abstract int V();
-    abstract int E();
-    abstract void show();
+public interface Graph<T> {
+    public void addEdge(T startVertex, T endVertex);
+    public Iterable<T> DFS(T startVertex);
+    public Iterable<T> BFS(T startVertex);
 }
